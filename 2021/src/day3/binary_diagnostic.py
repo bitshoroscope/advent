3c18dab03815b9531732c9e11ae4293bb406e52c
Andy <andres.bravo@propiedades.com>
2021-12-06 00:21:21 -0600

    Added correct solution for day3b

--- 2021/src/day3/binary_diagnostic.py
+++ 2021/src/day3/binary_diagnostic.py
@@ -14,28 +14,32 @@ def diagnostic():
         return gamma_rate * epsilon_rate
 
 def get_life_support_rating():
-    with open('../../resources/debug_input.txt') as f:
+    with open('../../resources/day3_input.txt') as f:
         lines = f.readlines()
         lines = [line.strip() for line in lines]
         transposed_lines_len = len(lines[0])
-        index = 0
-        index_bits = 0
-        filtered_lines = lines
-        while(index_bits < transposed_lines_len):
-            transposed_lines = transpose_input(filtered_lines)
-            counter_ones, counter_zeros = get_bit_counters(transposed_lines[index_bits])
-            filter_oxygen = '1' if counter_ones >= counter_zeros else '0'
-            filter_co2 = '0' if counter_zeros <= counter_ones else '1'
-            filtered_lines = [line for line in filtered_lines if line[index] == filter_oxygen]
-            if len(filtered_lines) == 1:
-                break
-            else:
-                index += 1
-            index_bits += 1
-        print(filtered_lines)
-        # filtered_oxygen = get_rating(lines, filters_oxygen)
-        # filtered_co2 = get_rating(lines, filters_co2)
-        # return int(filtered_oxygen,2) * int(filtered_co2,2)
+        filtered_oxy = get_indicator(lines, transposed_lines_len, "filter_oxygen")
+        filtered_co2 = get_indicator(lines, transposed_lines_len, "filter_co2")
+        return int(filtered_oxy,2) * int(filtered_co2,2)
+
+
+def get_indicator(filtered_lines, transposed_lines_len, filter_name):
+    index = 0
+    index_bits = 0
+    while index_bits < transposed_lines_len:
+        transposed_lines = transpose_input(filtered_lines)
+        counter_ones, counter_zeros = get_bit_counters(transposed_lines[index_bits])
+        if filter_name == 'filter_oxygen':
+            my_filter = '1' if counter_ones >= counter_zeros else '0'
+        else:
+            my_filter = '0' if counter_zeros <= counter_ones else '1'
+        filtered_lines = [line for line in filtered_lines if line[index] == my_filter]
+        if len(filtered_lines) == 1:
+            break
+        else:
+            index += 1
+        index_bits += 1
+    return filtered_lines[0]
 
 
 def transpose_input(read_lines):
