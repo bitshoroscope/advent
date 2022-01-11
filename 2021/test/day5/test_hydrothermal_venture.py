c73fb0f9decb585ecc6779348f1cb14457b1c949
Andy <bitshoroscope@gmail.com>
2022-01-11 01:27:57 -0600

    Added matrix generation with boundaries

--- 2021/test/day5/test_hydrothermal_venture.py
+++ 2021/test/day5/test_hydrothermal_venture.py
@@ -4,3 +4,12 @@ def test_parse_line():
 	vent = [[[0,9],[5,9]], [[8,0],[0,8]]]
 	input = ['0,9 -> 5,9\n', '8,0 -> 0,8']
 	assert vent == sol.get_points(input)
+
+def test_sequence_generator():
+	input = [[0,9],[5,9]]
+	sequence = sol.transform_to_sequence(input)
+	assert sequence == [[0,9],[1,9],[2,9],[3,9],[4,9],[5,9]]
+
+	input = [[0,1],[0,5]]
+	sequence = sol.transform_to_sequence(input)
+	assert sequence == [[0, 1], [0,2], [0,3], [0,4], [0,5]]
